7f2a6c42e97e04c09c44edf48ad6129aec6759a5
naveen-perera <naveen.perera@syscolabs.com>
2019-01-02 23:34:49 +0530

    GET and POST requets

--- xpress-demo/index.js
+++ xpress-demo/index.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+    {id:1 ,name:'course1'},
+    {id:2 ,name:'course2'}
+];
 
 app.get('/',(req, res) => {
-    res.send('Hello world!!!!');
+    res.send('Hello world!!!');
 });
 
 app.get('/api/courses',(req, res) => {
     res.send([1,2,3]);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    let course=courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('Not found here');
+    res.send(course);
+
+});
+
+app.post('/api/courses', (req,res) => {
+    let course = {
+        id: courses.length+1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query);
 });
 
 const port = process.env.PORT || 3000;
